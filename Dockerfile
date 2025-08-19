0c40a7a70f688be037c2fe8459eb2506515de259
wycreativestudio <wycreativestudio@gmail.com>
2025-08-19 23:31:03 +0800

    second commit

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,7 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
+EXPOSE 8080
+
 CMD ["python", "app.py"]
+
--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return "<h1>Hello World 🚀</h1><p>Web server Flask pertama kamu sudah jalan!</p>"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8080)
